d58dd9c21032b7fa99711bf642e0345e77eaa624
Hellsingoff <hellsingoff@gmail.com>
2019-11-02 23:44:59 +0500

    recursion fix

--- src/lab2/Lab2_04.java
+++ src/lab2/Lab2_04.java
@@ -1,23 +1,13 @@
 package lab2;
 
-import java.util.Arrays;
-
 class TwinFact {
 	public static int factRegular(int n) {
 		for (int r = n - 2; r > 0; r -= 2) { n *= r; }
 		return n;
 	}
-	public static int factRecurs(int ... n) {
-		if (n.length == 1) {
-			n  = Arrays.copyOf(n, n.length + 1);
-			n[1] = n[0];
-			n[0] -= 2;
-		} else if (n.length == 2) {
-			n[1] *= n[0];
-			n[0] -= 2;
-		}
-		if (n[0] > 0) factRecurs(n);
-		return n[1];
+	public static int factRecurs(int n) {
+		if ((n == 1) || (n == 2)) return n;
+		return n * factRecurs(n - 2);
 	}
 }
 
--- src/lab2/Lab2_05.java
+++ src/lab2/Lab2_05.java
@@ -1,22 +1,14 @@
 package lab2;
 
-import java.util.Arrays;
-
 class SumPower {
 	public static int regularSum(int n) {
 		int result = 0;
 		for (int i = n; i > 0; i--) result += Math.pow(i, 2);
 		return result;
 	}
-	public static int recursSum(int ... n) {
-		if (n.length == 1) {
-			n  = Arrays.copyOf(n, n.length + 1);
-			n[1] = (int) Math.pow(n[0]--, 2);
-		} else if (n.length == 2) {
-			n[1] += Math.pow(n[0]--, 2);
-		}
-		if (n[0] > 0) recursSum(n);
-		return n[1];
+	public static int recursSum(int n) {
+		if (n > 0) return (int) (Math.pow(n, 2) + recursSum(--n));
+		return 0;
 	}
 }
 
