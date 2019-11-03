f599a5d21baabfe01b94e9ca007326a5cec88ca4
Hellsingoff <hellsingoff@gmail.com>
2019-11-03 16:24:37 +0500

    Create Lab3_1.java

---
+++ src/lab3/Lab3_1.java
@@ -0,0 +1,27 @@
+package lab3;
+
+class OneText {
+	private String str;
+	OneText(String str) { this.str = str; }
+	public String toString() { return this.getClass().getSimpleName() + ": " + str;	}
+}
+
+class TwoText extends OneText {
+	private String str2;
+	TwoText(String str) {
+		super(str);
+		str2 = "";
+	}
+	TwoText(String str, String str2) {
+		super(str);
+		this.str2 = str2;
+	}
+	public String toString() { return super.toString() + ", " + str2; }
+}
+
+public class Lab3_1 {
+	public static void main(String[] args) {
+		System.out.println(new OneText("qwe").toString());
+		System.out.println(new TwoText("qwe", "rty").toString());
+	}
+}
